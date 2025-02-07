04b9c20be0620ca1015b9d896d45211927233bff
NikitaORL <nikita.orlenko234@gmail.com>
2025-02-07 09:43:04 +0200

    Update Protseduur.sql

--- Protseduur.sql
+++ Protseduur.sql
@@ -147,3 +147,118 @@ END;
 EXEC rahvaArvuUuendus 7, 1.2;
 
 drop procedure rahvaArvuUuendus
+--------------------------------------------------------------------------------
+CREATE database protseduurORL
+use protseduurORL
+create table linn(
+linnId int Primary Key identity (1,1),
+linnNimi varchar(30),
+rahvaArv int);
+
+SELECT * FROM linn 
+insert into linn (linnNimi, rahvaArv)
+values 
+('Tallinn', 440000),
+('Narva', 53000),
+('Tartu', 95000)
+
+create procedure lisalinn
+@lnimi varchar(30),
+@rArv int 
+AS
+BEGIN
+insert into linn (linnNimi, rahvaArv)
+values 
+(@lnimi, @rArv);
+SELECT * FROM linn
+END;
+
+EXEC lisalinn @lnimi='Tallinn234', @rArv=440000
+--lihtsam 
+EXEC lisalinn 'Saaremaa', 40000
+--kirje kustutamine
+
+ALTER TABLE linn (...
+SELECT * FROM linn;
+--uue veeru lisamine
+ALTER TABLE linn ADD test int;
+--veruu kustutamine
+ALTER TABLE linn DROP COLUMN test;
+
+CREATE PROCEDURE veeruLisaKustuta
+@valik varchar(20),
+@veerunimi varchar(20),
+@tyyp varchar(20)=null
+
+AS
+BEGIN
+declare @SQLtegevus as varchar(max)
+set @SQLtegevus=case
+when @valik='add' then concat ('ALTER TABLE linn ADD ',  @veerunimi, ' ', @tyyp)
+when @valik='drop' then concat ('ALTER TABLE linn DROP COULM ',  @veerunimi)
+END;
+print @SQLtegevus;
+begin
+EXEC (@SQLtegevus);
+END
+END;
+
+--kutse
+EXEC veeruLisaKustuta @valik='add',@veerunimi='test3', @tyyp='int';
+SELECT * FROM linn;
+EXEC veeruLisaKustuta @valik='drop', @veerunimi='test3';
+
+--------------------------------------------------------------
+
+
+CREATE PROCEDURE veeruLisaKustutaTabelis
+@valik varchar(20),
+@veerunimi varchar(20),
+@tabelinimi varchar(20),
+@tyyp varchar(20)=null
+
+AS
+BEGIN
+declare @SQLtegevus as varchar(max)
+set @SQLtegevus=case
+when @valik='add' then concat (' ALTER TABLE ', @tabelinimi, ' ADD ' ,  @veerunimi, ' ', @tyyp)
+when @valik='drop' then concat (' ALTER TABLE ', @tabelinimi, ' DROP CO ',  @veerunimi)
+END;
+print @SQLtegevus;
+begin
+EXEC (@SQLtegevus);
+END
+END;
+
+--kutse
+EXEC veeruLisaKustutaTabelis @valik='add', @tabelinimi='linn', @veerunimi='test3', @tyyp='int';
+SELECT * FROM linn;
+EXEC veeruLisaKustutaTabelis @valik='drop', @tabelinimi='linn', @veerunimi='test3';
+
+--protseduur tingimusega
+create procedure rahvaHinnanng
+@piir int
+
+
+AS
+BEGIN
+SELECT linnNimi, IIF(rahvaArv<@piir, 'väike linn', 'suur linn') AS Hinnang
+FROM linn;
+
+
+END;
+
+DROP PROCEDURE rahvaHinnanng
+
+EXEC rahvaHinnanng 100000;
+--Agregaat funktsioonid: sum(), AVERAGE - AVG(), MIN(), MAX(), COUNT()
+
+CREATE PROCEDURE kokkuRahvaArv
+
+AS
+BEGIN
+SELECT SUM(rahvaArv) AS 'kokku rahvaArv', AVG(rahvaArv) AS 'keskmine rahvaArv', COUNT(*) AS 'linnade arv' 
+FROM linn;
+END;
+
+EXEC kokkuRahvaArv;
